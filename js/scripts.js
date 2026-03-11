107bb94cd1958bbeb85ff0f7c44e7c9c9f4bce50
Xenoslogic <sdv666999@gmail.com>
2026-03-11 13:15:30 +0300

    ver-110826-1

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,73 @@ let logoDark = document.querySelector('.logo-dark');
 let mMenuToggle = document.querySelector('.mobile-menu-toggle');
 let menu = document.querySelector('.mobile-menu');
 
+const lightModeOn = (event) => {
+  navbar.classList.add('navbar-light');
+  logoLight.classList.add('visualy-hidden');
+  logoDark.classList.remove('visualy-hidden');
+};
+
+const darkModeOn = (event) => {
+  navbar.classList.remove('navbar-light');
+  logoLight.classList.remove('visualy-hidden');
+  logoDark.classList.add('visualy-hidden');
+}
+
+const openMenu = (event) => {
+  menu.classList.add('is-open');
+  mMenuToggle.classList.add('close-menu');
+  document.body.style.overflow = "hidden";
+  lightModeOn();
+};
+
+const closeMenu = (event) => {
+  menu.classList.remove('is-open');
+  mMenuToggle.classList.remove('close-menu');
+  document.body.style.overflow = "";
+  darkModeOn();
+};
+
 window.addEventListener('scroll', () => {
   if (this.scrollY > 1) {
-    navbar.classList.add('navbar-light');
-    logoLight.classList.add('visualy-hidden');
-    logoDark.classList.remove('visualy-hidden');
+    lightModeOn();
   } else {
-    navbar.classList.remove('navbar-light');
-    logoLight.classList.remove('visualy-hidden');
-    logoDark.classList.add('visualy-hidden');
+    darkModeOn();
   }
 })
 
 mMenuToggle.addEventListener('click', (event) => {
   event.preventDefault();
-  menu.classList.toggle('is-open')
+  if (menu.classList.contains('is-open')) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
 })
+
+const swiper = new Swiper('.swiper', {
+  speed: 400,
+  autoHeight: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      centeredSlides: true
+    },
+    // when window width is >= 480px
+    650: {
+      slidesPerView: 3,
+      centeredSlides: true
+    },
+    // when window width is >= 640px
+    1200: {
+      slidesPerView: 3,
+
+    }
+  },
+
+});
